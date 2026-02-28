11269ed6a529c80b7869a1c6bec565f3625ec149
Sozanskiy Illia <Sozanskiyilj@gmail.com>
2026-02-28 11:50:11 +0200

    added video ids extraction

--- data_extractions.py
+++ data_extractions.py
@@ -8,11 +8,9 @@ load_dotenv()
 
 api_key = os.getenv("YOUTUBE_API")
 chanel_handle = "MrBeast"
-
-BASE_URL = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={chanel_handle}&key={api_key}"
-
-
-def get_playlist_id(url):
+BASE_URL_PLAYLISTS_LIST = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={chanel_handle}&key={api_key}"
+max_results = 50 
+def get_playlist_id(url: str):
     """
     Function to parse JSON and get Playlist Id for further data exctracion
     """
@@ -29,5 +27,45 @@ def get_playlist_id(url):
     except requests.exceptions.HTTPError as e:
         raise e
 
+ 
+
+def get_video_ids(playlist_id, max_result):
+    
+    video_ids = []
+    pageToken = None
+    BASE_URL_VIDEO_LIST = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=ContentDetails&maxResults={max_result}&playlistId={playlist_id}&key={api_key}"
+
+    try : 
+        while True:
+            url = BASE_URL_VIDEO_LIST
+            
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+                
+            response = requests.get(url)
+            
+            response.raise_for_status()
+            
+            data = response.json()
+            
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+                
+            pageToken = data.get('nextPageToken')
+            
+            if not pageToken:
+                break
+            
+        return video_ids
+            
+            
+    except requests.exceptions.HTTPError as e:
+        raise e
+    
+
+
 if __name__ == "__main__":
-    pass 
+    playlist_id = get_playlist_id(BASE_URL_PLAYLISTS_LIST)
+    get_video_ids(playlist_id,max_results)
+    
